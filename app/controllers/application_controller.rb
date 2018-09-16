59251f9bf982a7dc07e34e8b7e873744d7036f6a
Nisha Salim <nocturne.noire@gmail.com>
2018-09-16 19:12:53 +0530

    Add ruby version file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, this is the toy app from railstutorials.org"
+  end
 end
